aa5115acf66d3a1635d75a3958895fce21274c9f
Rickard Karlsson <rickard@fmca.se>
2021-12-02 09:17:35 +0100

    Added solution for day1-task1

---
+++ day2/task1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+horizontal_inputs = []
+depth_inputs = []
+
+def get_int_from_string(str):
+    for word in str.split():
+        if word.isdigit():
+            return int(word)
+
+with open('inputs.txt', 'r') as file:
+    for line in file:
+        if 'up' in line:
+            depth_inputs.append(-abs(get_int_from_string(line)))
+        elif 'down' in line:
+            depth_inputs.append(get_int_from_string(line))
+        else:
+            horizontal_inputs.append(get_int_from_string(line))
+
+print(sum(horizontal_inputs) * sum(depth_inputs))
