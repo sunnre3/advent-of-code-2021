69205a8fffeace75ef0ee08d4d8202474fb9b947
Rickard Karlsson <rickard@fmca.se>
2021-12-05 22:00:35 +0100

    Added solution for day 4 task 1

---
+++ day4/task1.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+
+def boardHasBingo(board):
+    for row in board:
+        numOfMarked = 0
+        for number in row:
+            if number["marked"] == True:
+                numOfMarked += 1
+        if numOfMarked == len(row):
+            print('a', row)
+            return True
+
+    for pos in range(len(board[0])):
+        numOfMarked = 0
+        for row in board:
+            if row[pos]["marked"] == True:
+                numOfMarked += 1
+        if numOfMarked == len(board):
+            print('b', pos)
+            return True
+
+    return False
+
+numbers = []
+boards = []
+
+with open('./inputs.txt', 'r') as file:
+    numbers = list(map(int, file.readline().split(',')))
+    board = []
+    for line in file:
+        if len(line) > 1:
+            row = []
+            for num in line.strip().replace('  ', ' ').split(' '):
+                row.append({
+                    "number": int(num),
+                    "marked": False
+                })
+            board.append(row)
+        else:
+            if len(board) > 0:
+                boards.append(board)
+            board = []
+
+for calledNumber in numbers:
+    done = False
+    for board in boards:
+        for row in board:
+            for num in row:
+                if num["number"] == calledNumber:
+                    num["marked"] = True
+        if boardHasBingo(board):
+            print(calledNumber)
+            sumOfUnmarked = 0
+            for row in board:
+                for num in row:
+                    if num["marked"] == False:
+                        sumOfUnmarked += num["number"]
+            print(f"Sum of unmarked numbers times called number is: {sumOfUnmarked * calledNumber}")
+            done = True
+
+    if done:
+        break
+
